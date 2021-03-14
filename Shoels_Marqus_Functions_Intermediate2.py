e6cb1150c62b9ff7acfb7877900233fbb626d104
Marqus Shoels <77904838+MShoels@users.noreply.github.com>
2021-03-13 19:16:31 -0600

    Add files via upload

--- Shoels_Marqus_Functions_Intermediate2.py
+++ Shoels_Marqus_Functions_Intermediate2.py
@@ -1,43 +1,86 @@
-#1 Update Values in Dictionaries and Lists
+# 1 Update Values in Dictionaries and Lists
 
-x = [ [5,2,3], [10,8,9] ] 
+x = [[5, 2, 3], [10, 8, 9]]
 students = [
-    {'first_name':  'Michael', 'last_name' : 'Jordan'},
-    {'first_name' : 'John', 'last_name' : 'Rosales'}
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'}
 ]
 sports_directory = {
-    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+    'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer': ['Messi', 'Ronaldo', 'Rooney']
 }
-z = [ {'x': 10, 'y': 20} ]
+z = [{'x': 10, 'y': 20}]
 
-#Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 
 x[1][0] = 15
-print (x)
+print(x)
 
-#Change the last_name of the first student from 'Jordan' to 'Bryant'
+# Change the last_name of the first student from 'Jordan' to 'Bryant'
 
 students[0]['last_name'] = 'Bryant'
-print (students)
+print(students)
 
-#In the sports_directory, change 'Messi' to 'Andres'
+# In the sports_directory, change 'Messi' to 'Andres'
 
 sports_directory.update['soccer'][0] = 'Andres'
 print(sports_directory)
 
-#Change the value 20 in z to 30
-z [0]['y'] = 30
+# Change the value 20 in z to 30
+z[0]['y'] = 30
 print(z)
 
-#2 Iterate Through a List of Dictionaries Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value.
+# 2 Iterate Through a List of Dictionaries Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value.
 students = [
-    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'Michael', 'last_name': 'Jordan'},
     {'first_name': 'John', 'last_name': 'Rosales'},
     {'first_name': 'Mark', 'last_name': 'Guillen'},
     {'first_name': 'KB', 'last_name': 'Tonel'}
 ]
 
-def iterateDictionary (students)
-    for key in Students:
-    print (iterateDictionary(students))
+
+def iterate_dictionary(some_list):
+    for i in some_list:
+        print('first name=', i['first_name'], '- last name=', i['last_name'])
+
+
+iterate_dictionary(students)
+
+# 3 Get Values From a List of Dictionaries Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary.
+
+
+def iterate_Dictionary2(key, some_list):
+    for s in some_list:
+        print(s[key])
+
+
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+        {'first_name': 'John', 'last_name': 'Rosales'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+print('First Name:Students')
+iterate_Dictionary2('first_name', students)
+print('Last Name:Students')
+iterate_Dictionary2('last_name', students)
+
+# 4 Iterate Through a Dictionary with List Values Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list.
+
+
+def printInfo(some_dict):
+
+    print(len(some_dict['locations']), "LOCATIONS")
+    for location in some_dict['locations']:
+        print(location)
+    print()
+    print(len(some_dict['instructors']), "INSTRUCTORS")
+    for location in some_dict['instructors']:
+        print(location)
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+printInfo(dojo)
